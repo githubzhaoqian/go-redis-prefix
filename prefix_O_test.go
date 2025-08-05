7945a9048807f1077f130c73e8fd90f34b80430f
褚兆前 <1028290810@qq.com>
2025-08-05 13:46:31 +0800

    feat: test O command

---
+++ prefix_O_test.go
@@ -0,0 +1,80 @@
+package redisprefix
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func TestObjectEncoding(t *testing.T) {
+	// Url: https://redis.io/docs/latest/commands/object-encoding/
+	// Syntax: OBJECT ENCODING key
+	cmd := client.ObjectEncoding(testCtx, "ObjectEncodingKey")
+	_, err := cmd.Result()
+	if err != nil && err != redis.Nil {
+		t.Fatalf("cmd %v", err)
+	}
+	args := cmd.Args()
+	argsSrt := JoinInterfaceSliceBySpan(args...)
+	t.Log("cmd: ", argsSrt)
+	target := JoinInterfaceSliceBySpan("OBJECT ENCODING", prefixHook.formatKey("ObjectEncodingKey"))
+	if !strings.EqualFold(argsSrt, target) {
+		t.Fatalf("waning! %s != %s", argsSrt, target)
+	}
+	t.Log("Success")
+}
+
+func TestObjectFreq(t *testing.T) {
+	// Url: https://redis.io/docs/latest/commands/object-freq/
+	// Syntax: OBJECT FREQ key
+	cmd := client.ObjectFreq(testCtx, "ObjectFreqKey")
+	_, err := cmd.Result()
+	if err != nil && err != redis.Nil {
+		t.Fatalf("cmd %v", err)
+	}
+	args := cmd.Args()
+	argsSrt := JoinInterfaceSliceBySpan(args...)
+	t.Log("cmd: ", argsSrt)
+	target := JoinInterfaceSliceBySpan("OBJECT FREQ", prefixHook.formatKey("ObjectFreqKey"))
+	if !strings.EqualFold(argsSrt, target) {
+		t.Fatalf("waning! %s != %s", argsSrt, target)
+	}
+	t.Log("Success")
+}
+
+func TestObjectIdleTime(t *testing.T) {
+	// Url: https://redis.io/docs/latest/commands/object-idletime/
+	// Syntax: OBJECT IDLETIME key
+	cmd := client.ObjectIdleTime(testCtx, "ObjectIdleTimeKey")
+	_, err := cmd.Result()
+	if err != nil && err != redis.Nil {
+		t.Fatalf("cmd %v", err)
+	}
+	args := cmd.Args()
+	argsSrt := JoinInterfaceSliceBySpan(args...)
+	t.Log("cmd: ", argsSrt)
+	target := JoinInterfaceSliceBySpan("OBJECT IDLETIME", prefixHook.formatKey("ObjectIdleTimeKey"))
+	if !strings.EqualFold(argsSrt, target) {
+		t.Fatalf("waning! %s != %s", argsSrt, target)
+	}
+	t.Log("Success")
+}
+
+func TestObjectRefCount(t *testing.T) {
+	// Url: https://redis.io/docs/latest/commands/object-refcount/
+	// Syntax: OBJECT REFCOUNT key
+	cmd := client.ObjectRefCount(testCtx, "ObjectRefCountKey")
+	_, err := cmd.Result()
+	if err != nil && err != redis.Nil {
+		t.Fatalf("cmd %v", err)
+	}
+	args := cmd.Args()
+	argsSrt := JoinInterfaceSliceBySpan(args...)
+	t.Log("cmd: ", argsSrt)
+	target := JoinInterfaceSliceBySpan("OBJECT REFCOUNT", prefixHook.formatKey("ObjectRefCountKey"))
+	if !strings.EqualFold(argsSrt, target) {
+		t.Fatalf("waning! %s != %s", argsSrt, target)
+	}
+	t.Log("Success")
+}
