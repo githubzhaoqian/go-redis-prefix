fc667cafdbcaed1b17e97e91fb11e61e8fa55a08
褚兆前 <1028290810@qq.com>
2025-08-05 09:51:35 +0800

    feat: test I command

---
+++ prefix_I_test.go
@@ -0,0 +1,80 @@
+package redisprefix
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func TestIncr(t *testing.T) {
+	// Url: https://redis.io/docs/latest/commands/incr/
+	// Syntax: INCR key
+	cmd := client.Incr(testCtx, "INCRKey")
+	_, err := cmd.Result()
+	if err != nil && err != redis.Nil {
+		t.Fatalf("cmd %v", err)
+	}
+	args := cmd.Args()
+	argsSrt := JoinInterfaceSliceBySpan(args...)
+	t.Log("cmd: ", argsSrt)
+	target := JoinInterfaceSliceBySpan("INCR", prefixHook.formatKey("INCRKey"))
+	if !strings.EqualFold(argsSrt, target) {
+		t.Fatalf("waning! %s != %s", argsSrt, target)
+	}
+	t.Log("Success")
+}
+
+func TestIncrBy(t *testing.T) {
+	// Url: https://redis.io/docs/latest/commands/incrby/
+	// Syntax: INCRBY key increment
+	cmd := client.IncrBy(testCtx, "IncrByKey", 1)
+	_, err := cmd.Result()
+	if err != nil && err != redis.Nil {
+		t.Fatalf("cmd %v", err)
+	}
+	args := cmd.Args()
+	argsSrt := JoinInterfaceSliceBySpan(args...)
+	t.Log("cmd: ", argsSrt)
+	target := JoinInterfaceSliceBySpan("INCRBY", prefixHook.formatKey("IncrByKey"), 1)
+	if !strings.EqualFold(argsSrt, target) {
+		t.Fatalf("waning! %s != %s", argsSrt, target)
+	}
+	t.Log("Success")
+}
+
+func TestIncrByFloat(t *testing.T) {
+	// Url: https://redis.io/docs/latest/commands/incrbyfloat/
+	// Syntax: INCRBYFLOAT key increment
+	cmd := client.IncrByFloat(testCtx, "IncrByFloatKey", 1)
+	_, err := cmd.Result()
+	if err != nil && err != redis.Nil {
+		t.Fatalf("cmd %v", err)
+	}
+	args := cmd.Args()
+	argsSrt := JoinInterfaceSliceBySpan(args...)
+	t.Log("cmd: ", argsSrt)
+	target := JoinInterfaceSliceBySpan("INCRBYFLOAT", prefixHook.formatKey("IncrByFloatKey"), 1)
+	if !strings.EqualFold(argsSrt, target) {
+		t.Fatalf("waning! %s != %s", argsSrt, target)
+	}
+	t.Log("Success")
+}
+
+func TestInfo(t *testing.T) {
+	// Url: https://redis.io/docs/latest/commands/info/
+	// Syntax: INFO [section [section ...]]
+	cmd := client.Info(testCtx, "redis_version", "redis_git_sha1")
+	_, err := cmd.Result()
+	if err != nil && err != redis.Nil {
+		t.Fatalf("cmd %v", err)
+	}
+	args := cmd.Args()
+	argsSrt := JoinInterfaceSliceBySpan(args...)
+	t.Log("cmd: ", argsSrt)
+	target := JoinInterfaceSliceBySpan("INFO", "redis_version", "redis_git_sha1")
+	if !strings.EqualFold(argsSrt, target) {
+		t.Fatalf("waning! %s != %s", argsSrt, target)
+	}
+	t.Log("Success")
+}
