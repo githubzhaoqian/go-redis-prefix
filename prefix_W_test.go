28cf4b821be2ef8ff3c7f360564966ff91a5e0f1
褚兆前 <1028290810@qq.com>
2025-08-05 14:32:44 +0800

    feat: test W command

---
+++ prefix_W_test.go
@@ -0,0 +1,96 @@
+package redisprefix
+
+import (
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func TestWait(t *testing.T) {
+	// Url: https://redis.io/docs/latest/commands/wait/
+	// Syntax: WAIT numreplicas timeout
+	cmd := client.Wait(testCtx, 1, testTimeout)
+	_, err := cmd.Result()
+	if err != nil && err != redis.Nil {
+		t.Fatalf("cmd %v", err)
+	}
+	args := cmd.Args()
+	argsSrt := JoinInterfaceSliceBySpan(args...)
+	t.Log("cmd: ", argsSrt)
+	target := JoinInterfaceSliceBySpan("WAIT", 1, testFormatMs())
+	if !strings.EqualFold(argsSrt, target) {
+		t.Fatalf("waning! %s != %s", argsSrt, target)
+	}
+	t.Log("Success")
+}
+
+func TestWaitAOF(t *testing.T) {
+	// Url: https://redis.io/docs/latest/commands/waitaof/
+	// Syntax: WAITAOF numlocal numreplicas timeout
+	cmd := client.WaitAOF(testCtx, 1, 0, 0)
+	//_, err := cmd.Result()
+	//if err != nil && err != redis.Nil {
+	//	t.Fatalf("cmd %v", err)
+	//}
+	args := cmd.Args()
+	argsSrt := JoinInterfaceSliceBySpan(args...)
+	t.Log("cmd: ", argsSrt)
+	target := JoinInterfaceSliceBySpan("WAITAOF", 1, 0, 0)
+	if !strings.EqualFold(argsSrt, target) {
+		t.Fatalf("waning! %s != %s", argsSrt, target)
+	}
+	t.Log("Success")
+}
+
+func TestWatch(t *testing.T) {
+	// Url: https://redis.io/docs/latest/commands/watch/
+	// Syntax: WATCH key [key ...]
+	clientB := redis.NewClient(&redis.Options{
+		Addr: "127.0.0.1:6739",
+		DB:   0,
+	})
+	err := clientB.Ping(testCtx).Err()
+	if err != nil {
+		panic(err)
+	}
+	clientB.AddHook(prefixHook)
+	done := make(chan struct{})
+	key := "WatchKey"
+	// 客户端A开启WATCH
+	go func() {
+		defer close(done)
+		err := client.Watch(testCtx, func(tx *redis.Tx) error {
+			getCmd := tx.Get(testCtx, key)
+			val, _ := getCmd.Int()
+			getArgs := getCmd.Args()
+			argsSrt := JoinInterfaceSliceBySpan(getArgs...)
+			t.Log("get cmd: ", argsSrt)
+			// 等待B修改
+			time.Sleep(3 * time.Second)
+
+			// 事务尝试加1
+			_, err := tx.TxPipelined(testCtx, func(pipe redis.Pipeliner) error {
+				pipe.Set(testCtx, key, val+1, 0)
+				return nil
+			})
+			return err
+		}, key)
+		if err == redis.TxFailedErr {
+			t.Log("Success conflict")
+			return
+		} else if err != nil && err != redis.Nil {
+			t.Errorf("A Watch err: %v", err)
+			return
+		}
+		t.Errorf("no conflict")
+	}()
+	// 客户端B在A的WATCH后修改key
+	time.Sleep(1 * time.Second)
+	setCmd := clientB.Set(testCtx, key, 100, 0)
+	setArgs := setCmd.Args()
+	argsSrt := JoinInterfaceSliceBySpan(setArgs...)
+	t.Log("set cmd: ", argsSrt)
+	<-done
+}
